9f34cc8759cd65fee2b7833fbeef1f54c711e363
RamiroCabri <cabriramiro@gmail.com>
2023-05-28 13:04:08 -0300

    fin de ejercicio 2

--- operadores/Program.cs
+++ operadores/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace operadores
+﻿using System;
+
+namespace operadores
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Escriba su nombre y apellido: ");
+           /* Console.WriteLine("Escriba su nombre y apellido: ");
 
             string? nombre = Console.ReadLine();
 
@@ -40,9 +42,22 @@
             string? color = Console.ReadLine();
 
             Console.WriteLine("Auto>>>>>" + puertas + " puertas, " + ruedas + " ruedas, " + " de la marca " + marca + ", con el IVT " + itv + ".");
+           */
+
+            Console.WriteLine("Ejercicio 3");
+            Console.WriteLine("Veamos si el numero que ingrese es mas grande que 18");
+            int numerobase = 18;
+            int numero = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("el numero elegido es mas grande que el numero 18 =  " + (numero > numerobase));
+            Console.WriteLine("el numero elegido es mas pequeño que el numero 18 =  " + (numero < numerobase));
+            Console.WriteLine("el numero elegido es igual que el numero 18 =  " + (numero == numerobase));
+            
+            char letra = 'A';
 
 
-
+            Console.WriteLine((numerobase == 18 && numerobase > 10));
+            Console.WriteLine((numerobase == 18 || numerobase == 10));
 
 
 
